93dcb300913f84ab523274ad100fabb6e1307b8b
ruth10-haiti <ruthdieuveuille09@gmail.com>
2026-04-22 17:29:28 -0500

    Nettoyage complet

--- trimed_backend/settings.py
+++ trimed_backend/settings.py
@@ -24,6 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY=django-insecure-f4f5j7s&!r4j@y+qo(mc=l@039&%q&41+3rr_$dj51w5$&e_0j
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=True, cast=bool)
 
